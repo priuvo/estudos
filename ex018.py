f6b21d9bc5bab28296b3827fcbd7b8fc6c200710
priuvo <146988986+priuvo@users.noreply.github.com>
2023-11-07 20:14:37 -0300

    Add files via upload

--- ex018.py
+++ ex018.py
@@ -2,8 +2,8 @@
 import math
 a = int(input("Digite um ângulo: "))
 
-sen = math.sin(a)
-cos = math.cos(a)
-tg = math.tan(a)
+sen = math.sin(math.radians(a))
+cos = math.cos(math.radians(a))
+tg = math.tan(math.radians(a))
 
 print("Os valores de seno, cosseno e tangente do ângulo {}º são, respectivamente: {:.2f}, {:.2f} e {:.2f}.".format(a,sen,cos,tg))
